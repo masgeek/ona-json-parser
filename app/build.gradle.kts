7ff49fcc37522bec37b2d2bf17cbb3518dc564d9
Sammy Barasa <barsamms@gmail.com>
2020-10-13 10:48:37 +0300

    Corrected bad references after merge conflict resolution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,50 +1,25 @@
 plugins {
+    id("org.springframework.boot")
     kotlin("jvm")
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    kotlin("plugin.spring")
 }
 
 
-tasks.getByName<Jar>("jar") {
-    manifest {
-        attributes["Main-Class"] = "com.tsobu.parser.app.AppKt"
-    }
-}
-
-//tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-////    archiveBaseName.set("app-full")
-//    archiveClassifier.set("")
-////    archiveVersion.set("1.0")
-//}
-
-tasks {
-    build {
-//        dependsOn(shadowJar)
-    }
-}
-
-tasks.register("deploy") {
-    group = "Tsobu tasks"
-    description = "Produces a fat jar to run from terminal"
-
-    dependsOn("clean")
-    dependsOn("shadowJar")
-    doLast {
-        println("Deployment completed")
-    }
-}
 dependencies {
     implementation(project(":config"))
     implementation(project(":core"))
+    implementation(project(":enums"))
+    implementation(project(":database"))
 
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation(kotlin("reflect"))
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("com.sksamuel.hoplite:hoplite-core:1.3.6")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:1.3.6")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
