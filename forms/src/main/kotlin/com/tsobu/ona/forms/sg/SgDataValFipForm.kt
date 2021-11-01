607097b0926c161ce9e2596259c875d7414d6e39
Akilimo Machine <dev@tsobu.co.ke>
2021-11-01 07:56:41 +0300

    refactor: missing fields

--- forms/src/main/kotlin/com/tsobu/ona/forms/sg/SgDataValFipForm.kt
+++ forms/src/main/kotlin/com/tsobu/ona/forms/sg/SgDataValFipForm.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.ArrayList
 
-@JsonIgnoreProperties(ignoreUnknown = false)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class SgDataValFipForm {
     @JsonProperty("_id")
